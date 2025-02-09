54f8979d2d095031aa6ad1763a450418017595d2
Inesbouchibane <inesbouchibane.de@email.com>
2025-02-09 22:25:11 +0100

    modification du module graphisme vers environnement

---
+++ Code/environnement.py
@@ -0,0 +1,20 @@
+from robot import Robot
+from affichage import Affichage
+import pygame
+import math
+import random
+
+IR_MAX_DISTANCE = 100
+IR_SEUIL_ARRET = 50
+LARGEUR, HAUTEUR = 800, 600
+
+class Environnement:
+    def __init__(self, vitesse_gauche, vitesse_droite, mode):
+        self.robot = Robot(LARGEUR / 2, HAUTEUR / 2, vitesse_gauche, vitesse_droite)
+        self.mode = mode
+        self.avoidance_mode = False
+        self.avoidance_direction = None
+        self.obstacles = [(200, 200, 100, 100), (400, 100, 50,
+50)]
+        self.affichage = Affichage(LARGEUR, HAUTEUR, self.obstacles)
+
