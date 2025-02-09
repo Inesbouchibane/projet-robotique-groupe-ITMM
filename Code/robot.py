34fdff4b162cd662d77b298b80fbd1982a5cb5a9
bouzekrimohamed <lm_bouzekri@esi.dz>
2025-02-09 22:06:08 +0100

    ajout de la fonction infrarouge dans le module robot

--- Code/robot.py
+++ Code/robot.py
@@ -8,4 +8,11 @@ class Robot:
         self.angle = 0
         self.largeur, self.longueur = 20, 40
 
-
+    def scan_infrarouge(self, obstacles, max_distance):
+        angle_rad = math.radians(self.angle)
+        for d in range(0, int(max_distance), 5):
+            x_point, y_point = self.x + d * math.cos(angle_rad), self.y - d * math.sin(angle_rad)
+            for ox, oy, ow, oh in obstacles:
+                if ox < x_point < ox + ow and oy < y_point < oy + oh:
+                    return (x_point, y_point)
+        return (self.x + max_distance * math.cos(angle_rad), self.y - max_distance * math.sin(angle_rad))
