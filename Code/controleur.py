c0f9116bf02940264a72dda4f1b928501021543a
mouna2235677890 <mounabahamid10@gmail.com>
2025-02-10 19:01:03 +0100

    Ajout de la fonction verifier_limite_carre a notre classe Controleur

--- Code/controleur.py
+++ Code/controleur.py
@@ -15,3 +15,21 @@ class Controleur:
         self.vitesse_droite = vitesse_droite
         self.env.robot.vitesse_gauche = vitesse_gauche
         self.env.robot.vitesse_droite = vitesse_droite
+     
+    def verifier_limite_carre(self):
+        """
+        Vérifie si le robot a parcouru la distance correspondant à un côté du carré.
+        Si oui, on tourne à 90 degrés pour continuer le tracé du carré.
+        """
+        if self.distance_parcourue >= self.longueur_cote:
+            self.distance_parcourue = 0
+            self.etape_carre += 1
+            if self.etape_carre < 4:
+                # Tourner à 90 degrés
+                self.robot.vitesse_gauche = -2
+                self.robot.vitesse_droite = 2
+            else:
+                # Le carré est terminé
+                self.strategie = None
+                self.etape_carre = 0
+                self.robot.en_mouvement = False
