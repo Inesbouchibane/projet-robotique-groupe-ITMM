ffda4005f7a00e0a65c935b657795c185f6b7cf0
mouna2235677890 <mounabahamid10@gmail.com>
2025-02-11 20:26:19 +0100

    Ajout de la fct test_demarrer-simulation a test_controleur.py

--- Test/test_controleur.py
+++ Test/test_controleur.py
@@ -1,18 +1,16 @@
 import unittest
+from unittest.mock import MagicMock
 from controleur import Controleur
+from environnement import Environnement
 
 class TestControleur(unittest.TestCase):
+    
     def setUp(self):
-        self.controleur = Controleur(4, 5)
-
-    def test_initialisation(self):
-        self.assertEqual(self.controleur.vitesse_gauche, 4)
-        self.assertEqual(self.controleur.vitesse_droite, 5)
-
-    def test_ajuster_vitesse(self):
-        self.controleur.ajuster_vitesse(2, 3)
-        self.assertEqual(self.controleur.vitesse_gauche, 2)
-        self.assertEqual(self.controleur.vitesse_droite, 3)
-
-if __name__ == '__main__':
-    unittest.main()
+        self.env_mock = MagicMock(spec=Environnement)  # Mock de l'environnement
+        self.env_mock.robot = MagicMock()  # Mock du robot
+        self.controleur = Controleur(5, 5, "automatique")
+        self.controleur.env = self.env_mock  # Remplace l'environnement réel par un mock
+    
+    def test_demarrer_simulation(self):
+        self.controleur.demarrer_simulation()
+        self.env_mock.boucle_principale.assert_called_once()
