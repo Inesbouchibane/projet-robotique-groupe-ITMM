0339fb99e1941cf2950203a0e0c83394ab4f1341
mouna2235677890 <mounabahamid10@gmail.com>
2025-02-11 20:23:48 +0100

    Modification de notre fichier test_robot.py correspondant a notre nouveau code

--- Test/test_robot.py
+++ Test/test_robot.py
@@ -1,46 +1,21 @@
 import unittest
-from graphisme import Robot
+from robot import Robot
 
 class TestRobot(unittest.TestCase):
     def setUp(self):
-        self.robot = Robot(400, 300, 0, 0)
-    
-    def test_initialisation(self):
-        self.assertEqual(self.robot.x, 400)
-        self.assertEqual(self.robot.y, 300)
-        self.assertEqual(self.robot.vitesse_gauche, 0)
-        self.assertEqual(self.robot.vitesse_droite, 0)
-        self.assertEqual(self.robot.angle, 0)
-        self.assertTrue(self.robot.en_mouvement)
-    
-    def test_reinitialiser(self):
-        self.robot.x = 100
-        self.robot.y = 200
-        self.robot.vitesse_gauche = 5
-        self.robot.vitesse_droite = 5
-        self.robot.angle = 90
-        self.robot.en_mouvement = False
-
-        self.robot.reinitialiser()
-
-        self.assertEqual(self.robot.x, 400)
-        self.assertEqual(self.robot.y, 300)
-        self.assertEqual(self.robot.vitesse_gauche, 0)
-        self.assertEqual(self.robot.vitesse_droite, 0)
-        self.assertEqual(self.robot.angle, 0)
-        self.assertTrue(self.robot.en_mouvement)
+        self.robot = Robot(100, 100, 1, 1)
     
     def test_deplacement(self):
-        self.robot.vitesse_gauche = 2
-        self.robot.vitesse_droite = 2
-        initial_x = self.robot.x
-        initial_y = self.robot.y
-        
-        self.robot.x += (self.robot.vitesse_gauche + self.robot.vitesse_droite) / 2
-        self.robot.y += (self.robot.vitesse_gauche + self.robot.vitesse_droite) / 2
-        
-        self.assertNotEqual(self.robot.x, initial_x)
-        self.assertNotEqual(self.robot.y, initial_y)
+        x_avant, y_avant = self.robot.x, self.robot.y
+        self.robot.deplacer()
+        self.assertNotEqual((self.robot.x, self.robot.y), (x_avant, y_avant))
+    
+    def test_scan_infrarouge(self):
+        obstacles = [(200, 200, 50, 50)]
+        point_detecte = self.robot.scan_infrarouge(obstacles, 100)
+        self.assertIsInstance(point_detecte, tuple)
+        self.assertEqual(len(point_detecte), 2)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
+
