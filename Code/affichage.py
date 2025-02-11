2ac5cedf4441ab73e29eca883814ffc656dd27de
Takoua123 <160217744+Takoua123@users.noreply.github.com>
2025-02-11 22:10:02 +0100

    Update affichage.py

--- Code/affichage.py
+++ Code/affichage.py
@@ -1,29 +1,7 @@
 import pygame
 
 class Affichage:
-    def __init__(self, largeur, hauteur, obstacles):
-        self.largeur = largeur
-        self.hauteur = hauteur
-        self.obstacles = obstacles
-        pygame.init()
-        self.ecran = pygame.display.set_mode((largeur, hauteur))
-        self.font = pygame.font.Font(None, 24)  # Police pour afficher le texte
-
-    def mettre_a_jour(self, robot, ir_point, distance_ir, mode):
-        self.ecran.fill((255, 255, 255))  # Effacer l'écran
-        pygame.draw.circle(self.ecran, (0, 0, 255), (int(robot.x), int(robot.y)), 10)   # Dessiner le robot
-       
-        # Dessiner les obstacles
-        for ox, oy, ow, oh in self.obstacles:
-            pygame.draw.rect(self.ecran, (255, 0, 0), (ox, oy, ow, oh))
-
-        # Dessiner le point détecté par le capteur infrarouge
-        pygame.draw.circle(self.ecran, (0, 255, 0), (int(ir_point[0]), int(ir_point[1])), 5)
-
-	 # Dessiner la trajectoire du robot
-        if len(trajectoire) > 1:
-            pygame.draw.lines(self.ecran, (0, 0, 0), False, trajectoire, 2)  # Dessiner les lignes de la trajectoire
-
+    
         # Affichage des informations (mode et vitesse des roues)
         texte = f"Mode: {mode} | Vitesse Gauche: {robot.vitesse_gauche} | Vitesse Droite: {robot.vitesse_droite}"
         texte_surface = self.font.render(texte, True, (0, 0, 0))
